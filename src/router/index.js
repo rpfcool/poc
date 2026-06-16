d666a06cbc7eef7889a58718f5389c00ee47f5e8
Rodrigo <rpfcool@gmail.com>
2026-06-16 13:02:08 -0300

    altera roteador para modo Hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import KanbanView from '../views/KanbanView.vue'
 import BurndownView from '@/views/BurndownView.vue'
 import VelocityView from '@/views/VelocityView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
